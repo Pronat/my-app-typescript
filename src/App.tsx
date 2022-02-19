71d221019b81323dd564377ef1c4e5e09019d3e4
AlexPron <Fotolinza@gmail.com>
2022-02-19 21:55:09 +0300

    2

--- src/App.tsx
+++ src/App.tsx
@@ -3,71 +3,94 @@ import logo from './logo.svg';
 import './App.css';
 
 function hello() {
-    debugger;
+    console.log("hello rendered");
     alert("Hello IT-KAMASUTRA")
 }
+
 hello();
 
 function App() {
-    debugger
-  return (
-      <div>
-        This is APP component
-          <Star />
-      {/*<div className="App">*/}
-      {/*<header className="App-header">*/}
-      {/*  <img src={logo} className="App-logo" alt="logo" />*/}
-      {/*  <p>*/}
-      {/*    Edit <code>src/App.tsx</code> and save to reload.*/}
-      {/*  </p>*/}
-      {/*  <a*/}
-      {/*    className="App-link"*/}
-      {/*    href="https://reactjs.org"*/}
-      {/*    target="_blank"*/}
-      {/*    rel="noopener noreferrer"*/}
-      {/*  >*/}
-      {/*    Learn React*/}
-      {/*  </a>*/}
-      {/*</header>*/}
-    </div>
-  );
-}
-function Star() {
+    console.log("App rendered");
     return (
         <div>
-            Star
-            Star
-            Star
+            <AppTitle/>
+            <Star/>
+            <Star/>
+            <Accordion/>
+            <Rating/>
+            {/*<div className="App">*/}
+            {/*<header className="App-header">*/}
+            {/*  <img src={logo} className="App-logo" alt="logo" />*/}
+            {/*  <p>*/}
+            {/*    Edit <code>src/App.tsx</code> and save to reload.*/}
+            {/*  </p>*/}
+            {/*  <a*/}
+            {/*    className="App-link"*/}
+            {/*    href="https://reactjs.org"*/}
+            {/*    target="_blank"*/}
+            {/*    rel="noopener noreferrer"*/}
+            {/*  >*/}
+            {/*    Learn React*/}
+            {/*  </a>*/}
+            {/*</header>*/}
+        </div>
+    );
+}
 
+function AppTitle() {
+    console.log("AppTitle rendered");
+    return (
+        <> This is APP component </>
+    )
+}
+
+function Star() {
+    console.log("Star rendered");
+    return (
+        <div>
+            star
         </div>
     )
 }
 
 function Rating() {
-    debugger
+    console.log("Rating rendered");
     return (
-        <div>
-        <div>start</div>
-        <div>start</div>
-        <div>start</div>
-        <div>start</div>
-        <div>start</div>
-        </div>
+        <>
+            <Star/>
+            <Star/>
+            <Star/>
+        </>
     );
 }
 
 function Accordion() {
-    debugger
+    console.log("Accordion rendered");
     return (
         <div>
-            <h3>Меню</h3>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
+            <AccordionTitle/>
+            <AccordionBody/>
         </div>
     )
+}
+
+function AccordionTitle() {
+    console.log("AccordionTitle rendered");
+    return (
+        <h3>Меню</h3>
+    )
+}
+
+function AccordionBody() {
+    console.log("AccordionBody rendered");
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
+
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -5,21 +5,17 @@ import reportWebVitals from './reportWebVitals';
 import App from "./App";
 
 ReactDOM.render(
-  // <React.StrictMode>
-    <App />,
-  // </React.StrictMode>,
-  //   <div>Hello!</div>,
-  document.getElementById('root')
+    // <React.StrictMode>
+    <App/>,
+    // </React.StrictMode>,
+    //   <div>Hello!</div>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-
-
-
 
 
 // let a =  {
