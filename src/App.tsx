b5649f663efd32d136a36ebb0b5fd8d382541395
AlexPron <Fotolinza@gmail.com>
2022-03-20 10:51:46 +0300

    lesson5-1

--- src/App.tsx
+++ src/App.tsx
@@ -14,35 +14,10 @@ function App() {
     console.log("App rendered");
     return (
         <div>
-            {/*<PageTitle title={"This is APP component"}/>*/}
-            {/*<PageTitle title={"My friends"}/>*/}
-            {/*Article 1*/}
-            {/*<Rating value={3} />*/}
-            {/*<Star selected={true} />*/}
+
             <Accordion titleValue={"Menu"} collapsed={true}/>
             <Accordion titleValue={"Users"} collapsed={false}/>
-            {/*Article 2*/}
-            {/*<Rating value={0}/>*/}
-            {/*<Rating value={1}/>*/}
-            {/*<Rating value={2}/>*/}
-            {/*<Rating value={3}/>*/}
-            {/*<Rating value={4}/>*/}
-            {/*<Rating value={5}/>*/}
-            {/*<div className="App">*/}
-            {/*<header className="App-header">*/}
-            {/*  <img src={logo} className="App-logo" alt="logo" />*/}
-            {/*  <p>*/}
-            {/*    Edit <code>src/App.tsx</code> and save to reload.*/}
-            {/*  </p>*/}
-            {/*  <a*/}
-            {/*    className="App-link"*/}
-            {/*    href="https://reactjs.org"*/}
-            {/*    target="_blank"*/}
-            {/*    rel="noopener noreferrer"*/}
-            {/*  >*/}
-            {/*    Learn React*/}
-            {/*  </a>*/}
-            {/*</header>*/}
+
         </div>
     );
 }
