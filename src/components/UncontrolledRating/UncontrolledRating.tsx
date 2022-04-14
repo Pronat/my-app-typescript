db037106896cbf82cd30252edfa9876104ba5dbe
AlexPron <Fotolinza@gmail.com>
2022-04-14 23:38:57 +0300

    lesson7-callback1-1-1

--- src/components/UncontrolledRating/UncontrolledRating.tsx
+++ src/components/UncontrolledRating/UncontrolledRating.tsx
@@ -14,7 +14,7 @@ export function UncontrolledRating() {
             <Star selected={value > 1}/>
             <Star selected={value > 2}/>
             <Star selected={value > 3}/>
-            <Star selected={value > 4}/>
+            <Star selected={value > 4} />
         </div>
     )
 }
