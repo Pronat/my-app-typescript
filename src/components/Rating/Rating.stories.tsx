113d5ab82a17dfbc5005cfb0e4d933e12fb7fa6a
Pronat <Fotolinza@gmail.com>
2022-05-17 22:39:30 +0300

    lesson10-storyBook1-4

--- src/components/Rating/Rating.stories.tsx
+++ src/components/Rating/Rating.stories.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import {action} from '@storybook/addon-actions'
+import {Rating} from "./Rating";
 
 export default {
-    title: 'Button',
-    component: Button,
+    title: 'Rating stories',
+    component: Rating,
     }
 
-export const Text = () => <Button onClick={action('clicked')}>Hello Button</Button>
-export const Emoji = () => <Button onClick={action('clicked')}>
-    <span role="img" aria-label="so cool">
-        smile-smile
-    </span>
-</Button>
+export const Rating0 = () => <Rating value={0} onClick={x=>x} />
+export const Rating1 = () => <Rating value={1} onClick={x=>x} />
+export const Rating2 = () => <Rating value={2} onClick={x=>x} />
+export const Rating3 = () => <Rating value={3} onClick={x=>x} />
+export const Rating4 = () => <Rating value={4} onClick={x=>x} />
+export const Rating5 = () => <Rating value={5} onClick={x=>x} />
+
+
+
+
+
+
+
+
+    // export const Text = () => <Button onClick={action('clicked')}>Hello Button</Button>
+// export const Emoji = () => <Button onClick={action('clicked')}>
+//     <span role="img" aria-label="so cool">
+//         smile-smile
+//     </span>
+// </Button>
