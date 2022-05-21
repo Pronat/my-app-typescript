8adba8bd68ce663c415b7855eff2faacee0d139e
Pronat <Fotolinza@gmail.com>
2022-05-21 12:23:55 +0300

    lesson14-add array-4

--- src/components/Accordion/Accordion.stories.tsx
+++ src/components/Accordion/Accordion.stories.tsx
@@ -1,29 +1,28 @@
 import React, {useState} from 'react';
 import {action} from '@storybook/addon-actions'
 import Accordion, {AccordionPropsType} from "./Accordion";
-import {ComponentStory} from "@storybook/react";
-import {Button} from "../../stories/Button";
 
 export default {
     title: 'Accordion',
     component: Accordion,
     }
 const callback = action("accordion mode change event fired")
+const onClickCallback = action("some item was clicked")
 
 
 
 
 
 
-export const CollapsedMode = () => <Accordion titleValue={"Menu"} collapsed={true} onChange={callback} items={[]} />
-export const UnCollapsedMode = () => <Accordion titleValue={"Users"} collapsed={false} onChange={callback} items={["Dimich", "Valera", "Artem", "Evgen"]} />
+export const CollapsedMode = () => <Accordion titleValue={"Menu"} collapsed={true} onChange={callback} items={[]} onClick={onClickCallback} />
+export const UnCollapsedMode = () => <Accordion titleValue={"Users"} collapsed={false} onChange={callback} items={[{title:"Dimich", value:1}, {title:"Valera", value:2}, {title:"Artem", value:3}, {title:"Evgen", value:4}]} onClick={onClickCallback} />
 
 
 
 export const ModeChanging = () =>{
     const [change, setChange] = useState<boolean>(true)
     return(
-        <Accordion titleValue={"Users"} collapsed={change} onChange={()=>setChange(!change)} items={[{title:"Dimich", value:1}, {title:"Valera", value:2}, {title:"Artem", value:3}, {title:"Evgen", value:4}]} />
+        <Accordion titleValue={"Users"} collapsed={change} onChange={()=>setChange(!change)} items={[{title:"Dimich", value:1}, {title:"Valera", value:2}, {title:"Artem", value:3}, {title:"Evgen", value:4}]} onClick={onClickCallback} />
     )
 }
 
