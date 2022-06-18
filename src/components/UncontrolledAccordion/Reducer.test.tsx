f0767af15cb8e254afcaeb26792c04aab4244981
Pronat <Fotolinza@gmail.com>
2022-06-18 22:43:39 +0300

    lesson16-useReducer-16

---
+++ src/components/UncontrolledAccordion/Reducer.test.tsx
@@ -0,0 +1,12 @@
+import {reducer, StateType, TOGGLE_CONSTANT} from "./Reducer";
+
+test("test of reducer should be true", () => {
+    const state: StateType = {collapsed: false}
+    const newState = reducer(state, {type: TOGGLE_CONSTANT})
+    expect(newState.collapsed).toBe(true)
+})
+test("test of reducer should be false", () => {
+    const state: StateType = {collapsed: true}
+    const newState = reducer(state, {type: TOGGLE_CONSTANT})
+    expect(newState.collapsed).toBe(false)
+})
--- src/components/UncontrolledAccordion/Reducer.tsx
+++ src/components/UncontrolledAccordion/Reducer.tsx
@@ -8,9 +8,7 @@ export const TOGGLE_CONSTANT = "TOGGLE-COLLAPSED"
 export const reducer = (state: StateType, action: ActionType): StateType => {
     switch (action.type) {
         case TOGGLE_CONSTANT:
-            const stateCopy = {...state}
-            stateCopy.collapsed = !stateCopy.collapsed
-            return stateCopy
+            return {...state, collapsed: !state.collapsed}
         default:
             throw new Error('Invalid action')
     }
