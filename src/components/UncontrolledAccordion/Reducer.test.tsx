d53c75137050656887d9c743f556355cb9d440a9
Pronat <Fotolinza@gmail.com>
2022-06-18 22:48:38 +0300

    lesson16-useReducer-17

--- src/components/UncontrolledAccordion/Reducer.test.tsx
+++ src/components/UncontrolledAccordion/Reducer.test.tsx
@@ -5,8 +5,17 @@ test("test of reducer should be true", () => {
     const newState = reducer(state, {type: TOGGLE_CONSTANT})
     expect(newState.collapsed).toBe(true)
 })
+
 test("test of reducer should be false", () => {
     const state: StateType = {collapsed: true}
     const newState = reducer(state, {type: TOGGLE_CONSTANT})
     expect(newState.collapsed).toBe(false)
+})
+
+test("fake type", () => {
+    const state: StateType = {collapsed: true}
+    // const newState = reducer(state, {type: TOGGLE_CONSTANT})
+    expect(()=>{
+        reducer(state, {type: TOGGLE_CONSTANT})
+    }).toThrowError();
 })
