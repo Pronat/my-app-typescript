f2946160c379dd17391d48c5a53d1e3c4c784e5e
Pronat <Fotolinza@gmail.com>
2022-06-19 19:52:20 +0300

    lesson17-React.memo-3

--- src/stories/ReactMemo.stories.tsx
+++ src/stories/ReactMemo.stories.tsx
@@ -5,11 +5,12 @@ export default {
 const NewMessagesCounter = (props: any) => {
     return <div>{props.count}</div>
 }
-type UsersType =  {users: Array<string>}
-const Users = (props:UsersType) => {
+
+const Users = (props:{users: Array<string>}) => {
     return <div>{
-        {props.users.map((u,i)=> <div>{u}</div>)
-    }</div>
+        props.users.map((u, i) => <div>{u}</div>)
+    }
+    </div>
 }
 
 export const Example1 = () => {
