3b253efccaf555f6d30c3c12eb72134f4b2034f3
Pronat <Fotolinza@gmail.com>
2022-06-19 21:23:53 +0300

    lesson17-React.memo-6

--- src/stories/ReactMemo.stories.tsx
+++ src/stories/ReactMemo.stories.tsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import React, {useState} from "react";
 
 export default {
     title: 'React.memo demo',
@@ -8,17 +8,23 @@ const NewMessagesCounter = (props: { count: number }) => {
     return <div>{props.count}</div>
 }
 
-const Users = (props:{users: Array<string>}) => {
+const UsersSecret = (props:{users: Array<string>}) => {
+    console.log("USERS")
     return <div>{
         props.users.map((u, i) => <div key={i}>{u}</div>)
     }
     </div>
 }
 
+const Users = React.memo(UsersSecret)
+
 export const Example1 = () => {
     const [counter, setCounter] = useState(0)
-    const [users, setUsers] = useState(["Alex", "Anton", "Ornald"])
+    const [users, setUsers] = useState(["Alex", "Anton", "Ornald", "Bold"])
+
     return <>
+        <button onClick={() => {
+            setCounter(counter + 1)}}>+</button>
         <NewMessagesCounter count={counter} />
         <Users users={users} />
     </>
