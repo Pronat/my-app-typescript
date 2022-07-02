243678c76559261da3ae11570a691cce22cb7932
Pronat <Fotolinza@gmail.com>
2022-07-02 13:29:31 +0300

    lesson18-useMemo2

--- src/stories/UseMemo.stories.tsx
+++ src/stories/UseMemo.stories.tsx
@@ -8,16 +8,13 @@ export default {
 export const Example1 = () => {
     const [a, setA] = useState<number>(0)
     const [b, setB] = useState<number>(0)
-    return <>
-        <input value={a} onChange={(e) => setA(Number(e.currentTarget.value))}/>
+    let resultA = 1
+    let resultB = 1
 
-
-
-
-
-
-
-
-        <input value={b} onChange={(e) => setB(+(e.currentTarget.value))}/>
-    </>
+    for (let i = 1; i <= a; i++)
+        for
+            return <>
+                <input value={a} onChange={(e) => setA(Number(e.currentTarget.value))}/>
+                <input value={b} onChange={(e) => setB(+e.currentTarget.value)}/>
+            </>
 }
