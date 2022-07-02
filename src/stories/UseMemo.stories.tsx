35f5d0be148dba0b7a4e4a52c33d16ba258dba83
Pronat <Fotolinza@gmail.com>
2022-07-02 13:39:47 +0300

    lesson18-useMemo3

--- src/stories/UseMemo.stories.tsx
+++ src/stories/UseMemo.stories.tsx
@@ -6,15 +6,26 @@ export default {
 }
 
 export const Example1 = () => {
-    const [a, setA] = useState<number>(0)
-    const [b, setB] = useState<number>(0)
+    const [a, setA] = useState<number>(5)
+    const [b, setB] = useState<number>(5)
     let resultA = 1
     let resultB = 1
 
-    for (let i = 1; i <= a; i++)
-        for
-            return <>
-                <input value={a} onChange={(e) => setA(Number(e.currentTarget.value))}/>
-                <input value={b} onChange={(e) => setB(+e.currentTarget.value)}/>
-            </>
+    for (let i = 1; i <= a; i++) {
+        resultA = resultA * i
+    }
+    for (let i = 1; i <= b; i++) {
+        resultB = resultB * i
+    }
+    return <>
+        <input value={a} onChange={(e) => setA(Number(e.currentTarget.value))}/>
+        <input value={b} onChange={(e) => setB(+e.currentTarget.value)}/>
+        <hr/>
+        <div>
+            Result for a: {resultA}
+        </div>
+        <div>
+            Result for b: {resultB}
+        </div>
+    </>
 }
