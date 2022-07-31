0ca30c8e191d04e175365e17a869ed5ca6ca9996
AlexPron <Fotolinza@gmail.com>
2022-07-31 17:07:29 +0300

    useState2-4

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -12,19 +12,28 @@ export const SimpleExample = () => {
     console.log("SimpleExample")
 
     useEffect(() => {
-        console.log("useEffect")
+        console.log("useEffect every render")
         document.title = counter.toString()
-        // api.getUsers().then('')
-        // setInterval
-        // indexedDB
-        // document.getElementById
-        // document.title = "Users"
-    },[counter])
+    })
+ useEffect(() => {
+        console.log("useEffect only first render")
+        document.title = counter.toString()
+    }, [])
+ useEffect(() => {
+        console.log("useEffect first render and every counter change")
+        document.title = counter.toString()
+    }, [counter])
 
+    // api.getUsers().then('')
+    // setInterval
+    // indexedDB
+    // document.getElementById
+    // document.title = "Users"
 
     return <>
         Hello, {counter} {fake}
-        <button onClick={() => setFake(fake + 1)}>+</button>
+        <button onClick={() => setFake(fake + 1)}>fake+</button>
+        <button onClick={() => setCounter(counter + 1)}>counter+</button>
     </>
 
 }
