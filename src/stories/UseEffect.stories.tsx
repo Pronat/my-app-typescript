5cb1f62b582cc367c2a27fed8b8d284c3607cce3
AlexPron <Fotolinza@gmail.com>
2022-07-31 17:24:14 +0300

    useState2-5

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -37,3 +37,27 @@ export const SimpleExample = () => {
     </>
 
 }
+export const SimpleTimeoutExample = () => {
+    const [fake, setFake] = useState(1)
+    const [counter, setCounter] = useState(1)
+
+    console.log("SimpleExample")
+
+    useEffect(() => {
+        console.log("useEffect every render")
+        document.title = counter.toString()
+    })
+
+    // api.getUsers().then('')
+    // setInterval
+    // indexedDB
+    // document.getElementById
+    // document.title = "Users"
+
+    return <>
+        Hello, {counter} {fake}
+        <button onClick={() => setFake(fake + 1)}>fake+</button>
+        <button onClick={() => setCounter(counter + 1)}>counter+</button>
+    </>
+
+}
